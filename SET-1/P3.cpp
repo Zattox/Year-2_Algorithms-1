9ac44c773028edc87f6a0c61daf70579c97cfb2a
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-09-26 19:34:50 +0300

    Solved P3

--- SET-1/P3.cpp
+++ SET-1/P3.cpp
@@ -2,22 +2,36 @@
 
 using namespace std;
 
-void solve() {
-  string str, ans;
-  cin >> str;
-
-  vector<int> st;
-  for (int i = 0; i < (int)str.size(); ++i) {
-    if (str[i] == '[') {
-      st.push_back(i);
+string unpacking(string &str, int &i) {
+  string result;
+  int count = 0;
+  while (i < (int)str.size()){
+    if (isdigit(str[i])) {
+      count = str[i] - '0';
+    } else if (str[i] == '[') {
+      i += 1;
+      string substr = unpacking(str, i);
+      for (int j = 0; j < count; ++j) {
+        result += substr;
+      }
     } else if (str[i] == ']') {
-      int ind = st.back();
-      st.pop_back();
-      string substr = str.substr(ind + 1, i - ind);
-      
+      return result;
+    } else {
+      result += str[i];
     }
+    ++i;
   }
 
+  return result;
+}
+
+void solve() {
+  string str;
+  getline(cin, str);
+
+  int start = 0;
+  string ans = unpacking(str, start);
+  cout << ans;
 }
 
 int main() {
@@ -25,11 +39,7 @@ int main() {
   cin.tie(nullptr);
   cout.tie(nullptr);
 
-  int t = 1;
-  while (t > 0) {
-    solve();
-    --t;
-  }
+  solve();
 
   return 0;
 }
