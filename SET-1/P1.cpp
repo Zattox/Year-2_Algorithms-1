002dfb42d93cb12c5cacc6fee80488c3a3aac580
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-09-28 02:14:10 +0300

    Solved Test Group 1

--- SET-1/P1.cpp
+++ SET-1/P1.cpp
@@ -1,21 +1,264 @@
-#include <bits/stdc++.h>
+#include <stdexcept>
+#include <unordered_set>
+#include "list.h"
 
-using namespace std;
-
-void solve() {
-
+List::List() {
+  _size = 0;
+  head = nullptr;
+  tail = nullptr;
 }
 
-int main() {
-  ios_base::sync_with_stdio(false);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
+List::List(const List& other) {
+  _size = other._size;
+  head = new Node(other.head->value);
+  Node* cur = other.head->next;
+  Node* prev = head;
+  tail = head;
+  while (cur != nullptr) {
+    tail = new Node(cur->value);
+    tail->prev = prev;
+    prev->next = tail;
+    cur = cur->next;
+    prev = tail;
+  }
+}
 
-  int t = 1;
-  while (t > 0) {
-    solve();
-    --t;
+List::List(std::vector<int> array) {
+  _size = array.size();
+  head = new Node(array[0]);
+  Node* prev = head;
+  for (int i = 1; i < (int)_size; ++i) {
+    Node* cur = new Node(array[i]);
+    cur->prev = prev;
+    prev->next = cur;
+    prev = cur;
+    if (i == size() - 1) {
+      tail = cur;
+    }
+  }
+}
+
+List::~List() {
+  clear();
+}
+
+int List::front() {
+  return head->value;
+}
+
+
+int List::back() {
+  return tail->value;
+}
+
+void List::push_back(int value) {
+  Node* ptr = new Node(value);
+  if (tail == nullptr) {
+    head = ptr;
+    tail = ptr;
+  } else {
+    tail->next = ptr;
+    ptr->prev = tail;
+    tail = ptr;
+  }
+  ++_size;
+}
+
+void List::push_front(int value) {
+  Node* ptr = new Node(value);
+  if (head == nullptr) {
+    head = ptr;
+    tail = ptr;
+  } else {
+    head->prev = ptr;
+    ptr->next = head;
+    head = ptr;
+  }
+  ++_size;
+}
+
+void List::insert(Node* pos, int value) {
+  if (pos == nullptr || pos->value > size() || pos->value < 0) {
+    throw std::runtime_error("Incorrect position!");
+  }
+  int i = 0;
+  auto cur = head;
+  while (cur != tail && i < pos->value) {
+    cur = cur->next;
+    ++i;
   }
 
-  return 0;
+  Node* ptr = new Node(value);
+  if (cur->next == nullptr) {
+    cur->next = ptr;
+    ptr->prev = cur;
+  } else {
+    auto tmp = cur->next;
+    cur->next = ptr;
+    ptr->prev = cur;
+    tmp->prev = ptr;
+    ptr->next = tmp;
+  }
+  ++_size;
+}
+
+void List::pop_front() {
+  if (head == nullptr) {
+    return;
+  }
+
+  Node* ptr = head->next;
+  if (ptr != nullptr) {
+    ptr->prev = nullptr;
+  } else {
+    tail = nullptr;
+  }
+
+  delete head;
+  head = ptr;
+  --_size;
+}
+
+void List::pop_back() {
+  if (tail == nullptr) {
+    return;
+  }
+
+  Node* ptr = tail->prev;
+  if (ptr != nullptr) {
+    ptr->next = nullptr;
+  } else {
+    head = nullptr;
+  }
+
+  delete tail;
+  tail = ptr;
+  --_size;
+}
+
+void List::erase(Node* pos) {
+  if (pos == nullptr) {
+    throw std::runtime_error("Incorrect position!");
+  }
+
+  if (pos->prev != nullptr) {
+    pos->prev->next = pos->next;
+  } else {
+    head = pos->next;
+  }
+  if (pos->next != nullptr) {
+    pos->next->prev = pos->prev;
+  } else {
+    tail = pos->prev;
+  }
+
+  delete pos;
+  --_size;
+}
+
+void List::clear() {
+  while (head != nullptr) {
+    pop_front();
+  }
+  tail = nullptr;
+}
+
+void List::reverse() {
+  if (head == nullptr || head->next == nullptr) {
+    return;
+  }
+
+  Node* prev = nullptr;
+  Node* cur = head;
+  while (cur != nullptr) {
+    prev = cur->prev;
+    cur->prev = cur->next;
+    cur->next = prev;
+
+    cur = cur->prev;
+  }
+}
+
+void List::remove_duplicates() {
+  std::unordered_set<int> st;
+  Node* cur = head;
+
+  while (cur != nullptr) {
+    if (!st.count(cur->value)){
+      st.insert(cur->value);
+      cur = cur->next;
+    } else {
+      if (cur->prev != nullptr) {
+        cur->prev->next = cur->next;
+      }
+      if (cur->next != nullptr) {
+        cur->next->prev = cur->prev;
+      }
+      Node* tmp = cur;
+      cur = tmp->next;
+      delete tmp;
+      --_size;
+    }
+  }
+}
+
+void List::replace(int old_value, int new_value) {
+  auto cur = head;
+  while (cur != nullptr) {
+    if (cur->value == old_value) {
+      cur->value = new_value;
+    }
+    cur = cur->next;
+  }
+}
+
+void List::merge(const List& other) {
+  Node* ptr_other = other.head;
+  Node* cur = tail;
+  while (ptr_other != nullptr) {
+    cur->next = ptr_other;
+    cur = cur->next;
+    ptr_other = ptr_other->next;
+  }
+}
+
+bool List::check_cycle() const {
+  std::unordered_set<Node*> st;
+
+  Node* cur = head;
+  while (cur != nullptr) {
+    if (st.find(cur) != st.end()) {
+      return true;
+    }
+    st.insert(cur);
+    cur = cur->next;
+  }
+  return false;
+}
+
+size_t List::size() const {
+  return _size;
+}
+
+bool List::empty() const {
+  return (_size == 0);
+}
+
+void List::copy(const List& other) {
+  Node* cur = head;
+  Node* other_cur = other.head;
+  while (other_cur != nullptr) {
+    cur->value = other_cur->value;
+    if (cur->next == nullptr && other_cur->next != nullptr) {
+      cur->next = new Node(other_cur->next->value);
+      cur->next->prev = cur;
+    }
+    cur = cur->next;
+    other_cur = other_cur->next;
+  }
+  while (cur != nullptr) {
+    Node *tmp = cur;
+    cur = cur->next;
+    delete tmp;
+  }
 }
