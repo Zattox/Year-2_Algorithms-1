c2732ae756a50eee2ba8730802127b6de4bb234e
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-10-09 12:58:42 +0300

    Create files for SET_2

---
+++ SET_2/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  int t = 1;
+  while (t > 0) {
+    solve();
+    --t;
+  }
+}
