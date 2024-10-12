f9f21bd1f08f26394c9dfe4ead7bebc6057c5c9c
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-10-13 01:27:31 +0300

    Submit 1-2 test groups (3 points)

--- SET_2/P6b.cpp
+++ SET_2/P6b.cpp
@@ -1,12 +1,49 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+typedef long double ld;
+
+struct Point {
+  int x, y, z;
+};
+
+double dist(Point &a, Point &b) {
+  return hypot(a.x - b.x, a.y - b.y, a.z - b.z);
+}
+
+void naive_dist(vector<Point> &arr) {
+  double ans = 2e9;
+  int ind1 = 0, ind2 = 0;
+  for (int i = 0; i < arr.size(); ++i) {
+    for (int j = i + 1; j < arr.size(); ++j) {
+      double d = dist(arr[i], arr[j]);
+      if (ans > d) {
+        ans = d;
+        ind1 = i;
+        ind2 = j;
+      }
+    }
+  }
+
+  cout.precision(20);
+  cout << ans << "\n";
+  cout << ind1 + 1 << ' ' << ind2 + 1;
+}
 
 void solve() {
+  int n;
+  cin >> n;
+  vector<Point> arr(n);
+  for(int i = 0; i < n; ++i) {
+    cin >> arr[i].x >> arr[i].y >> arr[i].z;
+  }
 
+  naive_dist(arr);
 }
 
 int main() {
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
 
