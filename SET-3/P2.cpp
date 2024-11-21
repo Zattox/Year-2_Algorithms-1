fcfa3e7823cb37cd3ee7d5ebe1dae6736996c0f4
Zattox <vladcsgoogurechik@mail.ru>
2024-11-21 18:06:00 +0300

    Submit P2 (6/6)

--- SET-3/P2.cpp
+++ SET-3/P2.cpp
@@ -4,7 +4,34 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
+const int max_num = 1000000;
+
+vector<int> countingSort(vector<int> &arr, int n) {
+  vector<int> ans, cnt(2 * (max_num + 1));
+  for (int i = 0; i < n; ++i) {
+    ++cnt[arr[i] + max_num];
+  }
+  for (int num = 0; num <= cnt.size(); ++num) {
+    for (int i = 0; i < cnt[num]; ++i) {
+      ans.push_back(num - max_num);
+    }
+  }
+  return ans;
+}
+
 void solve() {
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (int i = 0; i < n; ++i) {
+    cin >> arr[i];
+  }
+
+  auto ans = countingSort(arr, n);
+  for (auto el : ans) {
+    cout << el << ' ';
+  }
+
 
 }
 
