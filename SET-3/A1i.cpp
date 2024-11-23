eda22375b9556d7bb07e197be5a0573f6b4431c5
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-11-24 02:53:47 +0300

    Submit A1i (5/5)

--- SET-3/A1i.cpp
+++ SET-3/A1i.cpp
@@ -1,11 +1,55 @@
 #include <bits/stdc++.h>
 
-typedef long long ll;
-typedef long double ld;
 using namespace std;
 
-void solve() {
+struct Point {
+  double x, y;
+};
 
+struct Circle {
+  double x, y, r, r2;
+};
+
+bool in_circle(Point p, Circle c) {
+  double dx = (p.x - c.x) * (p.x - c.x);
+  double dy = (p.y - c.y) * (p.y - c.y);
+  return (dx + dy) <= c.r2;
+}
+
+const int cnt_points = 1000000;
+
+void solve() {
+  Circle c1, c2, c3;
+  cin >> c1.x >> c1.y >> c1.r;
+  cin >> c2.x >> c2.y >> c2.r;
+  cin >> c3.x >> c3.y >> c3.r;
+
+  c1.r2 = c1.r * c1.r;
+  c2.r2 = c2.r * c2.r;
+  c3.r2 = c3.r * c3.r;
+
+  double max_x = max({c1.x + c1.r, c2.x + c2.r, c3.x + c3.r});
+  double min_x = max({c1.x - c1.r, c2.x - c2.r, c3.x - c3.r});
+  double max_y = min({c1.y + c1.r, c2.y + c2.r, c3.y + c3.r});
+  double min_y = min({c1.y - c1.r, c2.y - c2.r, c3.y - c3.r});
+  double area = (max_x - min_x) * (max_y - min_y);
+
+  std::mt19937 generator(13);
+  std::uniform_real_distribution<> distr_x(min_x, max_x);
+  std::uniform_real_distribution<> distr_y(min_y, max_y);
+
+  double cnt_good = 0;
+  for (int i = 0; i < cnt_points; ++i) {
+    double cur_x = distr_x(generator);
+    double cur_y = distr_y(generator);
+    Point p = {cur_x, cur_y};
+    if (in_circle(p, c1) && in_circle(p, c2) && in_circle(p, c3)) {
+      cnt_good += 1;
+    }
+  }
+
+  cout.precision(20);
+  cout << area * cnt_good / cnt_points;
 }
 
 int main() {
