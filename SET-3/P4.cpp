fb69b0fbfe1d8975d12b6401bdb768a64e9b1ed3
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-11-23 17:09:33 +0300

    Submit 4 (5/5)

--- SET-3/P4.cpp
+++ SET-3/P4.cpp
@@ -4,8 +4,44 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
-void solve() {
+pair<int, int> frac_to_pair(string x) {
+  int i = 0;
+  string numerator;
+  while (x[i] != '/') {
+    numerator += x[i];
+    ++i;
+  }
+  ++i;
+  string denominator;
+  while (i < x.size()){
+    denominator += x[i];
+    ++i;
+  }
+  return {atoi(numerator.c_str()), atoi(denominator.c_str())};
+}
 
+bool cmp(pair<int, int> p1, pair<int, int> p2) {
+  ll x1 = 1ll * p1.first * p2.second;
+  ll x2 = 1ll * p2.first * p1.second;
+
+  return x1 < x2;
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  vector<pair<int, int>> arr(n);
+  for (int i = 0; i < n; ++i) {
+    string x;
+    cin >> x;
+    arr[i] = frac_to_pair(x);
+  }
+
+  std::stable_sort(arr.begin(), arr.end(), cmp);
+
+  for (auto el : arr) {
+    cout << el.first << '/' << el.second << ' ';
+  }
 }
 
 int main() {
