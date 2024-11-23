bb63222693d46efae4f094860fb82cd57230f999
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-11-23 16:56:03 +0300

    Submit P3 (6/6)

--- SET-3/P3.cpp
+++ SET-3/P3.cpp
@@ -4,84 +4,58 @@ typedef long long ll;
 typedef long double ld;
 using namespace std;
 
-string dec_to_bin(int x) {
-  string ans;
-  while (x > 0) {
-    ans.push_back('0' + x % 2);
-    x /= 2;
-  }
-  std::reverse(ans.begin(), ans.end());
-  return ans;
-}
-
-int bin_to_dec(string x) {
-  std::reverse(x.begin(), x.end());
-  int ans = 0;
-  int k = 1;
-  for (auto el : x) {
-    ans += (el - '0') * k;
-    k = (k << 1);
-  }
-  return ans;
-}
-
-void filling_zeros(string &x, int n) {
-  std::reverse(x.begin(), x.end());
-  while (x.size() < n) {
-    x.push_back('0');
-  }
-  std::reverse(x.begin(), x.end());
-}
-
-int sort_by_digit(vector<string> &arr, int ind, int prev_half) {
-  vector<string> additional_arr(arr.size());
-  int l = 0, r = (int)arr.size() - 1;
-
-  for (int i = 0; i < prev_half; ++i) {
-    auto el = arr[i];
-    if (el[ind] == '0') {
-      additional_arr[l++] = el;
-    } else {
-      additional_arr[r--] = el;
-    }
-  }
-
-  for (int i = (int)arr.size() - 1; i >= prev_half; --i) {
-    auto el = arr[i];
-    if (el[ind] == '0') {
-      additional_arr[l++] = el;
-    } else {
-      additional_arr[r--] = el;
-    }
-  }
-
-  arr.swap(additional_arr);
-  return l;
+int next_bit(pair<int, int> &num){
+  int bit = num.first % 2;
+  num.first = (num.first >> 1);
+  return bit;
 }
 
 vector<int> radixSort(vector<int> &a) {
-  int n = (int)a.size(), max_sz = 0;
-  vector<string> binary_a(n);
+  int n = (int)a.size(), max_sz = 32;
+  int cnt_negative = 0;
+  vector<pair<int, int>> arr(n);
   for (int i = 0; i < n; ++i) {
-    binary_a[i] = dec_to_bin(a[i]);
-    max_sz = max(max_sz, (int)binary_a[i].size());
-  }
-  for (int i = 0; i < n; ++i) {
-    filling_zeros(binary_a[i], max_sz);
+    arr[i] = {a[i], i};
+    if (a[i] < 0)
+      ++cnt_negative;
   }
 
-  int half = n;
-  for (int i = max_sz - 1; i >= 0; --i) {
-    half = sort_by_digit(binary_a, i, half);
+  int prev_half = n;
+  vector<pair<int, int>> additional_arr(n);
+  for (int ind = 0; ind < max_sz; ++ind) {
+    int l = 0, r = n - 1;
+    for (int i = 0; i < prev_half; ++i) {
+      if (next_bit(arr[i]) == 0)
+        additional_arr[l++] = arr[i];
+      else
+        additional_arr[r--] = arr[i];
+    }
+    for (int i = n - 1; i >= prev_half; --i) {
+      if (next_bit(arr[i]) == 0)
+        additional_arr[l++] = arr[i];
+      else
+        additional_arr[r--] = arr[i];
+    }
+    arr.swap(additional_arr);
+    prev_half = l;
   }
 
   int ch = 0;
-  vector<int> ans(n);
-  for (int i = 0; i < half; ++i) {
-    ans[ch++] = bin_to_dec(binary_a[i]);
+  vector<int> radix_ans(n);
+  for (int i = 0; i < prev_half; ++i) {
+    radix_ans[ch++] = a[arr[i].second];
   }
-  for (int i = n - 1; i >= half; --i) {
-    ans[ch++] = bin_to_dec(binary_a[i]);
+  for (int i = n - 1; i >= prev_half; --i) {
+    radix_ans[ch++] = a[arr[i].second];
+  }
+
+  ch = 0;
+  vector<int> ans(n);
+  for (int i = n - cnt_negative; i < n; ++i) {
+    ans[ch++] = radix_ans[i];
+  }
+  for (int i = 0; i < n - cnt_negative; ++i) {
+    ans[ch++] = radix_ans[i];
   }
 
   return ans;
