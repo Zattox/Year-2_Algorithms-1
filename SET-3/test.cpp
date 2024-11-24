72fdb01680a6eb9ffb2582715c95db65bdac7507
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-11-25 00:00:17 +0300

    Remove files

--- SET-3/test.cpp
+++ SET-3/test.cpp
@@ -1,11 +1,37 @@
 #include <bits/stdc++.h>
 
-typedef long long ll;
-typedef long double ld;
 using namespace std;
 
-void solve() {
+void insertionSort(vector<int> &arr) {
+  int n = arr.size();
+  for (int i = 1; i < n; ++i) {
+    int elem = arr[i];
+    int j = i - 1;
+    while (j >= 0 && arr[j] > elem) {
+      arr[j + 1] = arr[j];
+      --j;
+    }
+    arr[j + 1] = elem;
+  }
+}
 
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  for (int i = 0; i < n; ++i) {
+    int el;
+    cin >> el;
+    arr[i] = el;
+  }
+
+  auto start_time = std::chrono::steady_clock::now();
+  insertionSort(arr);
+  auto end_time = std::chrono::steady_clock::now();
+  auto elapsed_ns = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+  long double res = elapsed_ns.count() / 1000.0;
+  cout.precision(10);
+  cout << fixed << res << "\n";
 }
 
 int main() {
