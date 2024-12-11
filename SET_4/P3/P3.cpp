f81c8a21d231be18ab7d5be294eef43b2e1ce3e5
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2024-12-12 02:27:23 +0300

    Tests for P3

---
+++ SET_4/P3/P3.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+#include "release.cpp"
+
+typedef long long ll;
+typedef long double ld;
+using namespace std;
+
+void solve() {
+  AVLTree T;
+  for (int i = 0; i < 10; ++i)  {
+    cout << "Find " << i << " : " << T.find(i + 1) << "\n";
+    T.insert(i + 1);
+    cout << "Find " << i << " : " << T.find(i + 1) << "\n";
+    cout << "--------------------------------------\n";
+  }
+
+  auto x = T.traversal();
+  cout << "arr = ";
+  for (int i = 0; i < 10; ++i) {
+    cout << x[i] << ' ';
+  }
+  cout << "\n";
+
+  cout << T.empty() << "\n";
+  for (int i = 0; i < 11; ++i)  {
+    cout << "size = " << T.getSize() << " height = " << T.getHeight() << "\n";
+    T.erase(i);
+    cout << "size = " << T.getSize() << " height = " << T.getHeight() << "\n";
+    cout << "============================================\n";
+  }
+  cout << T.empty() << "\n";
+
+
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  int t = 1;
+  // cin >> t;
+  while (t > 0) {
+    solve();
+    --t;
+  }
+}
