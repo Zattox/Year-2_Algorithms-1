df0abccff469ca78067abf3ae4d317ad7f8677bd
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-02-05 22:38:43 +0300

    Submit P2 int SET-5 (12/12)

--- SET-5/P2/P2.cpp
+++ SET-5/P2/P2.cpp
@@ -1,22 +1,49 @@
 #include "bloom_filter.h"
 
-BloomFilter::BloomFilter(size_t, size_t) {
+BloomFilter::BloomFilter(size_t countOfHashes, size_t countOfBits)
+    : _hashes(countOfHashes), _bits(countOfBits) {
+  _filter.resize(countOfBits, false);
 }
 
-void BloomFilter::add(const std::string &) {
+void BloomFilter::add(const std::string &value) {
+  _raw_set.insert(value);
+  for (int i = 0; i < _hashes; ++i) {
+    size_t cur_hash = _get_hash(i, value);
+    _filter[cur_hash] = true;
+  }
 }
 
-bool BloomFilter::verify(const std::string &) {
+bool BloomFilter::verify(const std::string &value) {
+  ++cnt;
+  for (int i = 0; i < _hashes; ++i) {
+    size_t cur_hash = _get_hash(i, value);
+    if (!_filter[cur_hash]) {
+      return false;
+    }
+  }
+
+  if (_raw_set.count(value) == 0) {
+    ++false_positive_cnt;
+  }
+  return true;
 }
 
 double BloomFilter::getFPRate() const {
+  if (cnt == 0) {
+    return 0;
+  }
+  return 1.0 * false_positive_cnt / cnt;
 }
 
 size_t BloomFilter::numberOfHashFunctions() const {
+  return _hashes;
 }
 
 size_t BloomFilter::numberOfBits() const {
+  return _bits;
 }
 
-inline size_t BloomFilter::_get_hash(int, const std::string &) const {
+inline size_t BloomFilter::_get_hash(int hashNumber, const std::string &value) const {
+  std::string valueWithSalt = value + std::to_string(hashNumber);
+  return hasher(valueWithSalt) % _bits;
 }
