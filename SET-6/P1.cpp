89fdd7e33a7e6bc7f65cf935e741d9ed40f3fcdb
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-02-21 18:04:46 +0300

    Add template in all files

--- SET-6/P1.cpp
+++ SET-6/P1.cpp
@@ -1,3 +1,21 @@
-//
-// Created by dumpl on 21.02.2025.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+void solve() {
+
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t = 1;
+  //cin >> t;
+  while (t > 0) {
+    solve();
+    --t;
+  }
+}
--- SET-6/main.cpp
+++ SET-6/main.cpp
@@ -1,6 +1,21 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+
+void solve() {
+
+}
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
-  return 0;
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t = 1;
+  //cin >> t;
+  while (t > 0) {
+    solve();
+    --t;
+  }
 }
