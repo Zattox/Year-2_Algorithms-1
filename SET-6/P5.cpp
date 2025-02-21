c856194aa4936c1041dd97c26d1c42dde329a951
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-02-22 00:36:18 +0300

    Submit P5 (6/6)

--- SET-6/P5.cpp
+++ SET-6/P5.cpp
@@ -4,8 +4,38 @@ using namespace std;
 typedef long long ll;
 typedef long double ld;
 
-void solve() {
+const ll INF = 1e18;
 
+void solve() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<vector<ll>> dist(n, vector<ll>(n, INF));
+  for (int i = 0; i < m; ++i) {
+    int u, v, w;
+    cin >> u >> v >> w;
+    dist[u][v] = w;
+  }
+
+  for (int k = 0; k < n; ++k) {
+    for (int i = 0; i < n; ++i) {
+      for (int j = 0; j < n; ++j) {
+        dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+      }
+    }
+  }
+
+  for (int i = 0; i < n; ++i) {
+    for (int j = 0; j < n; ++j) {
+      if (i == j) {
+        continue;
+      }
+      if (dist[i][j] == INF) {
+        dist[i][j] = -1;
+      }
+      cout << i << ' ' << j << ' ' << dist[i][j] << "\n";
+    }
+  }
 }
 
 int main() {
