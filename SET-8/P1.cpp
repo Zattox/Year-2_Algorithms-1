027b557726af2f6181c3835ad4a2524995413c61
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-04-24 00:17:18 +0300

    Submit P1 (6/6)

--- SET-8/P1.cpp
+++ SET-8/P1.cpp
@@ -1,3 +1,37 @@
-//
-// Created by dumpl on 23.04.2025.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+void solve() {
+  string s;
+  cin >> s;
+
+  int n = s.length();
+  vector<int> p(n, 0);
+  for (int i = 1; i < n; ++i) {
+    int cur = p[i - 1];
+    while (cur > 0 && s[i] != s[cur]) {
+      cur = p[cur - 1];
+    }
+    if (s[i] == s[cur])
+      ++cur;
+    p[i] = cur;
+  }
+
+  for (auto el : p) {
+    cout << el << ' ';
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t = 1;
+  //cin >> t;
+  while (t > 0) {
+    solve();
+    --t;
+  }
+}
