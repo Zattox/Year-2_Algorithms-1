68bb8b6fa1b05cd67eb80123c3ec53d62d725cd4
Teplyakov Vladislav Vitalievich <somi.saaw@mail.ru>
2025-04-24 00:18:10 +0300

    Submit P5 (9/9)

--- SET-8/P5.cpp
+++ SET-8/P5.cpp
@@ -1,3 +1,48 @@
-//
-// Created by dumpl on 23.04.2025.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+void solve(string &str1, string &str2) {
+  string s1 = '/' + str1, s2 = '/' + str2;
+  int n = s1.length(), m = s2.length();
+
+  vector<vector<int>> dp(n, vector<int>(m));
+  for (int i = 0; i < n; ++i) {
+    dp[i][0] = i;
+  }
+  for (int i = 0; i < m; ++i) {
+    dp[0][i] = i;
+  }
+
+  for (int i = 1; i < n; ++i) {
+    for (int j = 1; j < m; ++j) {
+      if (s1[i] == s2[j]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      }
+      else {
+        dp[i][j] = min(dp[i - 1][j - 1] + 1, min(dp[i - 1][j], dp[i][j - 1]) + 1);
+      }
+    }
+  }
+
+  cout << dp[n - 1][m - 1] << ' ';
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  string t_str;
+  getline(cin, t_str);
+  int t = atoi(t_str.c_str());
+
+  while (t > 0) {
+    string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2);
+    solve(s1, s2);
+    --t;
+  }
+}
