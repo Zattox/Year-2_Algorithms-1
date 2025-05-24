a816bb041c8511dab124b7d4f24434d01fadf6ea
Zattox <vladcsgoogurechik@mail.ru>
2025-05-24 19:00:34 +0300

    Submit P2 (8/8)

--- SET-9/P2.cpp
+++ SET-9/P2.cpp
@@ -1,3 +1,52 @@
-//
-// Created by dumpl on 23.05.2025.
-//
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve() {
+  string input;
+  getline(cin, input);
+
+  unordered_map<string, int> stringToCode;
+  for (int i = 0; i < 128; i++) {
+    string s(1, static_cast<char>(i));
+    stringToCode[s] = i;
+  }
+
+  int nextCode = 128;
+  vector<int> codes;
+  string w = "";
+
+  for (char c : input) {
+    string wc = w + c;
+    if (stringToCode.find(wc) != stringToCode.end()) {
+      w = wc;
+    } else {
+      if (!w.empty()) {
+        codes.push_back(stringToCode[w]);
+      }
+      stringToCode[wc] = nextCode;
+      nextCode++;
+      w = string(1, c);
+    }
+  }
+
+  if (!w.empty()) {
+    codes.push_back(stringToCode[w]);
+  }
+
+  cout << codes.size() << "\n";
+  for (size_t i = 0; i < codes.size(); i++) {
+    cout << codes[i];
+    if (i < codes.size() - 1) {
+      cout << " ";
+    }
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
+  solve();
+}
