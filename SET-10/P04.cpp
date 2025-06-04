ac33211c6c460eb704ade35fe5724de7332f0a39
Zattox <vladcsgoogurechik@mail.ru>
2025-06-04 20:21:34 +0300

    Submit P04 (5/5)

--- SET-10/P04.cpp
+++ SET-10/P04.cpp
@@ -1,9 +1,21 @@
 #include <bits/stdc++.h>
 
+typedef long long ll;
 using namespace std;
 
-void solve() {
+const ll MOD = 1e9 + 7;
 
+void solve() {
+  int n;
+  cin >> n;
+  vector<ll> dp(n + 1, 0);
+  dp[0] = 1;
+  dp[1] = 2;
+  for (int i = 2; i <= n; ++i) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+  }
+
+  cout << dp[n];
 }
 
 int main() {
