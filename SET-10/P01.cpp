cad2f4da1531b67f2f64f341129cc58e6abbf7f0
Zattox <vladcsgoogurechik@mail.ru>
2025-06-04 20:06:11 +0300

    Submit P01 (4/4)

--- SET-10/P01.cpp
+++ SET-10/P01.cpp
@@ -1,9 +1,46 @@
 #include <bits/stdc++.h>
 
+typedef long long ll;
+typedef long double ld;
 using namespace std;
 
-void solve() {
+struct Item {
+  ll value;
+  ll weight;
+  double ratio;
+};
 
+void solve() {
+  int n;
+  ll W;
+  cin >> n >> W;
+
+  vector<Item> items(n);
+  for (int i = 0; i < n; i++) {
+    cin >> items[i].value >> items[i].weight;
+    items[i].ratio = (double) items[i].value / (double) items[i].weight;
+  }
+
+  sort(items.begin(), items.end(), [](const Item &a, const Item &b) {
+    return a.ratio > b.ratio;
+  });
+
+  ld total_value = 0.0;
+  ll remaining = W;
+
+  for (int i = 0; i < n && remaining > 0; i++) {
+    if (items[i].weight <= remaining) {
+      total_value += items[i].value;
+      remaining -= items[i].weight;
+    } else {
+      double fraction = (double) remaining / (double) items[i].weight;
+      total_value += items[i].value * fraction;
+      remaining = 0;
+    }
+  }
+
+  cout.precision(3);
+  cout << fixed << total_value << "\n";
 }
 
 int main() {
