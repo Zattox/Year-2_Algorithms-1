b803c48f179d621d271d82d52b24e858b9a9a300
Zattox <vladcsgoogurechik@mail.ru>
2025-06-07 16:02:09 +0300

    Submit P02 (6/6)

--- SET-10/P02.cpp
+++ SET-10/P02.cpp
@@ -3,7 +3,33 @@
 using namespace std;
 
 void solve() {
+  int n, l, r;
+  cin >> n >> l >> r;
 
+  int len = n;
+  vector<int> v(n);
+  --l;
+  --r;
+
+  int pick = (r + l) / 2;
+  v[pick] = n--;
+
+  int pos = pick, s = 1;
+  while (n > 0) {
+    pos += s;
+    if (pos >= 0 && pos < len) {
+      v[pos] = n--;
+    }
+    if (s > 0) {
+      s = -s - 1;
+    } else {
+      s = -s + 1;
+    }
+  }
+
+  for (int i = 0; i < len; ++i) {
+    cout << v[i] << ' ';
+  }
 }
 
 int main() {
